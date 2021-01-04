01e1df202ad217fd3c889dacb2ac5a06f9e6eb28
michelangelo <mickeylimalopes@gmail.com>
2021-01-04 13:37:04 -0300

    project ds_deliver created

---
+++ backend/src/test/java/com/sds2/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds2.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
