74d5606147e8f1cc923f72705c56a218ff3c608e
michelangelo <mickeylimalopes@gmail.com>
2021-01-10 14:38:55 -0300

    finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
-const API_URL = 'http://localhost:8080';
+//const localHost_URL = 'http://localhost:8080'
+const heroku_URL = 'https://michelangelo-sds2.herokuapp.com'
+
+const API_URL = heroku_URL
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts(){
@@ -9,4 +13,8 @@ export function fetchProducts(){
 
 export function fetchLocalMapBox(local: string){
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayload){
+    return axios.post(`${API_URL}/orders`, payload)
 }
