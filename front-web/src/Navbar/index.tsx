c7dca80a382a51ab0a3dbd79400f7c943c9118bf
michelangelo <mickeylimalopes@gmail.com>
2021-01-10 15:49:03 -0300

    adding navbar folder

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+import { ReactComponent as Logo} from './logo.svg';
+import { Link } from 'react-router-dom';
+//essa lib permite a utilização de svg
+
+function Navbar(){
+    return(
+        <nav className="main-navbar">
+            <Logo />
+            <Link to="/" className="logo-text">DS Delivery</Link>
+        </nav>
+    )
+}
+
+export default Navbar;
